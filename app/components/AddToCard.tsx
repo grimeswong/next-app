0e954e3afc49eaee98c3f59fe0cf72fb878ad6f2
grimeswong <grimeswongatwork@gmail.com>
2023-09-15 13:33:35 +1000

    2.7 - Client and Server Components

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const AddToCard = () => {
+  return (
+    <button onClick={() => console.log('Click')}>Add to Cart</button>
+  )
+}
+
+export default AddToCard
