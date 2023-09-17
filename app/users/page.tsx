b3ce28fdbd0e6b057a5acc08dd2b805cfc4dca7b
grimeswong <grimeswongatwork@gmail.com>
2023-09-17 21:00:04 +1000

    3.5 - DaisyUI

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,23 +3,35 @@ import React from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users",
-  { cache: 'no-store'}); // to keep use fetch fresh data
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  }); // to keep use fetch fresh data
   // { next: {revalidate: 10 } });   // to get fresh data form backend every 10 seconds
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>  {/* the time only be updated for development mode, not the production mode */}
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      {/* the time only be updated for development mode, not the production mode */}
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </>
   );
 };
