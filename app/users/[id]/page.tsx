016954e31840c2755609627e553236340bc9d12f
grimeswong <grimeswongatwork@gmail.com>
2023-09-18 22:46:11 +1000

    4.10  - Handling Not Found Errors

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,10 +1,13 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface Props {
-    params: { id: number }
+  params: { id: number };
 }
 
-const UserDetailPage = ({ params: { id }}: Props) => {
+const UserDetailPage = ({ params: { id } }: Props) => {
+  if (id > 10) notFound(); // navigate to the general not found page if not-found page isn't define in this folder
+
   return <div>UserDetailPage {id}</div>;
 };
 
