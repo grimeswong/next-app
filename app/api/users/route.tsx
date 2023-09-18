f4a5dc6a998ec66f587e32bb1e07d1813f49cd86
grimeswong <grimeswongatwork@gmail.com>
2023-09-19 00:13:56 +1000

    5.4 - Creating an Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -10,4 +10,14 @@ export function GET(request: NextRequest) {
 }
 
 // POST - creating data
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Validate
+  // If invalid, return 400,
+  // Else, return
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
+
 // PUT - updating data
