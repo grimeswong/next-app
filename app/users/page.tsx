4c24796ea7192a77239094894592fe029be67b86
grimeswong <grimeswongatwork@gmail.com>
2023-09-18 22:30:39 +1000

    4.9 - Showing Loading UIs

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,18 +1,21 @@
-import Link from 'next/link';
-import React from "react";
+import Link from "next/link";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
 
 interface Props {
-  searchParams: { sortOrder: string }
+  searchParams: { sortOrder: string };
 }
 
-const UserPage = async ({searchParams: { sortOrder } }: Props) => {
-
+const UserPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
-      <Link href="/users/new" className='btn'>New User</Link>
-      <UserTable sortOrder={sortOrder}/>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </>
   );
 };
