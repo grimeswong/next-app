265a953d3cf0941656071cfa91df95828211d62d
grimeswong <grimeswongatwork@gmail.com>
2023-09-22 18:53:23 +1000

    5.5 - Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -16,3 +16,24 @@ export function GET(
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Mosh" });
 }
+
+// PUT - updating data
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id: 1, name: body.name });
+}
+
+// Steps:
+// Validate the request body
+// If invalide, return 400
+// Fetch the user with the given id
+// If doesn't exist, return 404
+// Update the user
+// Return the udpated user
