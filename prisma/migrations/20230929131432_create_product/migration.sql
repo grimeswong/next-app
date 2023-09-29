471ba0b6de798b601b0b26c3c85c70cae285f36e
grimeswong <grimeswongatwork@gmail.com>
2023-09-29 23:28:41 +1000

    6.11 - Exercise - Storing Products

---
+++ prisma/migrations/20230929131432_create_product/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Double`.
+
+*/
+-- DropIndex
+DROP INDEX `Product_name_key` ON `Product`;
+
+-- AlterTable
+ALTER TABLE `Product` MODIFY `price` DOUBLE NOT NULL;
