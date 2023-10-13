4c2edf937a6440e9b5c0adff62b4d8c61de73a54
grimeswong <grimeswongatwork@gmail.com>
2023-10-13 17:12:33 +1000

    9.3 - Creating an Email Template

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  // Define inline Props object
+  return (
+    <Html>
+      <Preview>Welcome aboard!</Preview>
+      <Body>
+        <Container>
+          <Text>Hello {name}</Text>
+          <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default WelcomeTemplate;
